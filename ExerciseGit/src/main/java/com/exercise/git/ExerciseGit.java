bebfd3aa8692611e3929fdfd6dab81a40b0e86ea
MuoTie <596139547@qq.com>
2023-03-21 21:14:59 +0800

    主干添加

--- ExerciseGit/src/main/java/com/exercise/git/ExerciseGit.java
+++ ExerciseGit/src/main/java/com/exercise/git/ExerciseGit.java
@@ -5,5 +5,6 @@ public class ExerciseGit {
         System.out.println("Exercise Git");
         System.out.println("更新1");
         System.out.println("创建分支");
+        System.out.println("主干添加");
     }
 }
