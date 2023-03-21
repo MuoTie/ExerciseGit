3a4ae8e96bf0de6414a3e9e39be24eb4c4780792
MuoTie <596139547@qq.com>
2023-03-21 19:47:07 +0800

    更新1

--- ExerciseGit/src/main/java/com/exercise/git/ExerciseGit.java
+++ ExerciseGit/src/main/java/com/exercise/git/ExerciseGit.java
@@ -3,5 +3,6 @@ package com.exercise.git;
 public class ExerciseGit {
     public static void main(String[] args) {
         System.out.println("Exercise Git");
+        System.out.println("更新1");
     }
 }
