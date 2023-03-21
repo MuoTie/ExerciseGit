4c9a463594676982c1727d700d1f65478ab324ca
MuoTie <596139547@qq.com>
2023-03-21 19:06:56 +0800

    更新2

--- helloGit/src/main/java/com/exercise/git/HelloGit.java
+++ helloGit/src/main/java/com/exercise/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git更新");
+        System.out.println("Hello Git更新2");
     }
 }
